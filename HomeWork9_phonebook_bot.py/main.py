0ccec4c67939a6ca556c9207f2b7622ba579f0d8
Elena <keaya@rambler.ru>
2022-11-06 15:27:16 +0300

    добавила комментарии

--- HomeWork9_phonebook_bot.py/main.py
+++ HomeWork9_phonebook_bot.py/main.py
@@ -1,14 +1,18 @@
+# Импортируем необходимые классы.
 from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters
 
 import model
 
-bot_token = '5690495817:AAEl1eYizdmrmp09S7xBoIOuC_SpEDBlWzM'
+bot_token = 'Сюда пишем токен'
 bot = Bot(bot_token)
 updater = Updater(bot_token, use_context=True)
 dispatcher = updater.dispatcher
 
-add_storage = []
+add_storage = []    #   вреенный список для хранения инфо
+
+# Напишем соответствующие функции.
+# Их сигнатура и поведение аналогичны обработчикам текстовых сообщений.
 
 def start(update, context):
     context.bot.send_message(
@@ -18,8 +22,14 @@ def start(update, context):
         f"/add - Добавить контакт\n"
         f"/find - Найти контакт\n"
         f"/delete - Удалить контакт\n")
-
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, сам бот и класс updater, принявший сообщение.
 def watch(update, context):  
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
     data = model.read_csv()
     context.bot.send_message(
         update.effective_chat.id,
@@ -38,7 +48,6 @@ def find_surname(update, context):
         update.effective_chat.id,
         f"{res}")
 
-
 def add(update, context):
     context.bot.send_message(
         update.effective_chat.id,
@@ -52,7 +61,6 @@ def add_name(update, context):
         update.effective_chat.id,
         f"Введите фамилию")
     return 2
-
 
 def add_surname(update, context):
     surname = update.message.text
@@ -89,14 +97,22 @@ def stop(update, context):
     update.message.reply_text("Всего доброго!")
     return ConversationHandler.END
 
+
+# Handlers - обработчики сообщений
+ # Создаём обработчик сообщений типа Filters.text
+  
+    # После регистрации обработчика в диспетчере
+    # эта функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообений.
+  
 add_handler = ConversationHandler(
-    entry_points=[CommandHandler('add', add)],
+    entry_points=[CommandHandler('add', add)],   # Состояние внутри диалога.
     states={
         1: [MessageHandler(Filters.text & ~Filters.command, add_name)],
         2: [MessageHandler(Filters.text & ~Filters.command, add_surname)],
         3: [MessageHandler(Filters.text & ~Filters.command, add_phone)],
-    },
-    fallbacks=[CommandHandler('stop', stop)]
+    },# Точка прерывания диалога. В данном случае — команда /stop.
+    fallbacks=[CommandHandler('stop', stop)] 
 )
 
 find_handler = ConversationHandler(
@@ -120,10 +136,14 @@ start_handler = CommandHandler('start', start)
 back_handler = CommandHandler('back', start)
 watch_handler = CommandHandler('watch', watch)
 
+# Dispatсher - отвечает за вызов обработчика сообщений
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(watch_handler)
 dispatcher.add_handler(find_handler)
 dispatcher.add_handler(add_handler)
 dispatcher.add_handler(delete_handler)
+    # Запускаем цикл приема и обработки сообщений.
 updater.start_polling()
+ # Ждём завершения приложения.
+    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
 updater.idle()
